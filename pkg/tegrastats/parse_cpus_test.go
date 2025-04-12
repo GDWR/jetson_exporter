72c500852c214480250c5c7399fe598c461980b2
GDWR <gregory.dwr@gmail.com>
2025-04-12 14:40:27 +0100

    Add failing test and update parsing to fix

--- pkg/tegrastats/parse_cpus_test.go
+++ pkg/tegrastats/parse_cpus_test.go
@@ -28,5 +28,22 @@ func TestParseCPUs(t *testing.T) {
 				{Percentage: 63, Frequency: 102},
 			},
 		},
+		{
+			"04-11-2025 10:36:27 RAM 4321/62801MB (lfb 1759x4MB) SWAP 19/31400MB (cached 0MB) CPU [0%@729,0%@729,0%@729,0%@729,0%@729,0%@729,0%@729,0%@729,0%@729,0%@729,0%@729,0%@729] EMC_FREQ 0%@2133 GR3D_FREQ 0%@[0,0] VIC_FREQ 729 APE 174 CV0@-256C CPU@44.562C Tboard@32C SOC2@40.093C Tdiode@33C SOC0@41.093C CV1@-256C GPU@-256C tj@44.343C SOC1@41.25C CV2@-256C VDD_GPU_SOC 2340mW/2340mW VDD_CPU_CV 390mW/390mW VIN_SYS_5V0 4025mW/4025mW NC 0mW/0mW VDDQ_VDD2_1V8AO 301mW/301mW NC 0mW/0mW",
+			[]TegraCPU{
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+				{Percentage: 0, Frequency: 729},
+			},
+		},
 	})
 }
