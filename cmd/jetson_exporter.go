c533d201d02b6c5efe5eeeff31ce7a78a896dfbd
GDWR <gregory.dwr@gmail.com>
2025-04-12 16:03:55 +0100

    Update dependencies

--- cmd/jetson_exporter.go
+++ cmd/jetson_exporter.go
@@ -8,16 +8,15 @@ import (
 	"strconv"
 
 	"github.com/alecthomas/kingpin/v2"
-	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/promslog"
+	"github.com/prometheus/common/promslog/flag"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
+	"github.com/prometheus/exporter-toolkit/web/kingpinflag"
 
 	"github.com/gdwr/jetson_exporter/pkg/tegrastats"
-	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/prometheus/common/promlog"
-	webflag "github.com/prometheus/exporter-toolkit/web/kingpinflag"
 )
 
 var (
@@ -102,17 +101,17 @@ var (
 
 func main() {
 	var (
-		webConfig      = webflag.AddFlags(kingpin.CommandLine, ":9102")
+		webConfig      = kingpinflag.AddFlags(kingpin.CommandLine, ":9102")
 		metricsPath    = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
 		tegrastatsPath = kingpin.Flag("tegrastats.path", "Path to tegrastats binary, if not provided will attempt to use $PATH").Default("tegrastats").String()
 	)
 
-	promlogConfig := &promlog.Config{}
-	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	promslogConfig := &promslog.Config{}
+	flag.AddFlags(kingpin.CommandLine, promslogConfig)
 	kingpin.Version(version.Print("jetson-exporter"))
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
-	logger := promlog.New(promlogConfig)
+	logger := promslog.New(promslogConfig)
 
 	prometheus.Register(tegrastatsTemperature)
 	prometheus.Register(tegrastatsCPULoad)
@@ -125,20 +124,20 @@ func main() {
 	prometheus.Register(tegrastatsGR3DUsage)
 	prometheus.Register(tegrastatsGR3DFrequency)
 
-	level.Info(logger).Log("msg", "Starting jetson-exporter", "tegraPath", *tegrastatsPath)
+	logger.Info("Starting jetson-exporter", "tegraPath", *tegrastatsPath)
 
 	go func() {
 		cmd := exec.Command(*tegrastatsPath)
 
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
-			level.Error(logger).Log("Can't find tegrastats")
+			logger.Error("Can't find tegrastats")
 			panic(err)
 		}
 
 		err = cmd.Start()
 		if err != nil {
-			level.Error(logger).Log("Can't find tegrastats")
+			logger.Error("Can't find tegrastats")
 			panic(err)
 		}
 
@@ -147,13 +146,12 @@ func main() {
 			content := scanner.Text()
 			stats, err := tegrastats.ParseTegraStats(content)
 			if err != nil {
-				level.Error(logger).
-					Log("msg", "Error parsing tegrastats, provide this log in a Github Issue for resolution. https://github.com/GDWR/jetson-exporter/issues/new",
-						"content", content,
-						"err", err)
+				logger.Error("Error parsing tegrastats, provide this log in a Github Issue for resolution. https://github.com/GDWR/jetson-exporter/issues/new",
+					"content", content,
+					"err", err)
 				continue
 			}
-			level.Debug(logger).Log("message", "updating metrics")
+			logger.Debug("updating metrics")
 
 			for _, temp := range stats.Temps {
 				tegrastatsTemperature.WithLabelValues(temp.Name).Set(float64(temp.Temp))
@@ -196,7 +194,7 @@ func main() {
 
 	srv := &http.Server{}
 	if err := web.ListenAndServe(srv, webConfig, logger); err != nil {
-		level.Error(logger).Log("msg", "Error starting HTTP server", "err", err)
+		logger.Error("Error starting HTTP server", "err", err)
 		os.Exit(1)
 	}
 }
